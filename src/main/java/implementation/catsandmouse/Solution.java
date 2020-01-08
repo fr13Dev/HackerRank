6f18c62d53b3c7d3cb1e4af69cc1f8f04f231e0f
nikolai beliaev <fr13.dev@gmail.com>
2020-01-08 19:52:13 +0300

    cats and mouse

---
+++ src/main/java/implementation/catsandmouse/Solution.java
@@ -0,0 +1,21 @@
+package implementation.catsandmouse;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //System.out.println(catAndMouse(1, 2, 3));
+        System.out.println(catAndMouse(1, 3, 2));
+    }
+
+    static String catAndMouse(int x, int y, int z) {
+        final int distanceXToZ = Math.abs(z - x);
+        final int distanceYToZ = Math.abs(z - y);
+        if (distanceXToZ < distanceYToZ) {
+            return "Cat A";
+        } else if (distanceXToZ > distanceYToZ) {
+            return "Cat B";
+        } else {
+            return "Mouse C";
+        }
+    }
+}
